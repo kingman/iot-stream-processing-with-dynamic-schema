fb623c20ff1e9c0a142def7b75cd3c9f82fd2635
Charlie Wang <charlie.zwang@gmail.com>
2021-04-26 11:56:23 +0200

    add auto prepare script

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -14,14 +14,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -o nounset
-
+if [ -z "${GOOGLE_CLOUD_PROJECT}" ]; then
+    echo 'The GOOGLE_CLOUD_PROJECT environment variable that points to the default Google Cloud project is not defined. Terminating...'
+    exit 1
+fi
+export GOOGLE_CLOUD_REGION=europe-west1
+export GOOGLE_CLOUD_ZONE=europe-west1-d
 export TF_SERVICE_ACCOUNT_NAME=tf-service-account
 export TF_STATE_PROJECT=${GOOGLE_CLOUD_PROJECT}
 export TF_STATE_BUCKET=tf-state-bucket-${TF_STATE_PROJECT}
 export IOT_REGISTRY_ID=device-registry
 export IOT_DEVICE_ID=device-one
 export DATAFLOW_TEMPLATE_BUCKET=stream-processing-${GOOGLE_CLOUD_PROJECT}
-export BIGQUERY_DATASET_ID=iot-events
-export BIGQUERY_METRICS_TABLE_ID=measurements
+export BIGQUERY_DATASET_ID=iot_event
+export BIGQUERY_METRICS_TABLE_ID=message
 export BIGQUERY_UNKNOWN_MESSAGE_TABLE_ID=unkown_message
