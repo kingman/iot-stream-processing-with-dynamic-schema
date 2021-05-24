8cbeb8174aca57b8dc7812c9961d63cf222357b1
Charlie Wang <charlie.zwang@gmail.com>
2021-05-24 11:57:23 +0200

    fix pip install issue

--- tests/run.sh
+++ tests/run.sh
@@ -68,7 +68,7 @@ fi
 VENV_DIR=venv
 if [ ! -d "${VENV_DIR}" ]; then
     echo "Set up python virtual environment."
-    python3 -m venv ${VENV_DIR} && source ${VENV_DIR}/bin/activate && pip3 install -r requirements.txt && deactivate
+    python3 -m venv ${VENV_DIR} && source ${VENV_DIR}/bin/activate && CRYPTOGRAPHY_DONT_BUILD_RUST=1 pip3 install -r requirements.txt && deactivate
 fi
 
 source ${VENV_DIR}/bin/activate && python3 sim_device.py
